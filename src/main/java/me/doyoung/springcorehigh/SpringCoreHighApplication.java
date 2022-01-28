3f2a2f214fb9b2e34017f62c13d8739c2cea1991
김도영 <doyoung0205@naver.com>
2022-01-28 14:44:54 +0900

    Initial commit

---
+++ src/main/java/me/doyoung/springcorehigh/SpringCoreHighApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.springcorehigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreHighApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreHighApplication.class, args);
+    }
+
+}
