0bb6015ea49028db53c703240ef4c6c4141930bf
김도영 <doyoung0205@naver.com>
2022-01-28 20:44:55 +0900

    feat : add hello trace v2

---
+++ src/test/java/me/doyoung/springcorehigh/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,24 @@
+package me.doyoung.springcorehigh.trace.hellotrace;
+
+import me.doyoung.springcorehigh.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV2Test {
+    @Test
+    void begin_end() {
+        final HelloTraceV2 trace = new HelloTraceV2();
+        final TraceStatus status1 = trace.begin("hello");
+        final TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception() {
+        final HelloTraceV2 trace = new HelloTraceV2();
+        final TraceStatus status1 = trace.begin("hello");
+        final TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
