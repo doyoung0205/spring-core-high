bff7348524660c265a085fa1fba50d669f80df24
김도영 <doyoung0205@naver.com>
2022-01-28 17:44:11 +0900

    feat : add order mvc v1

---
+++ src/main/java/me/doyoung/springcorehigh/app/v1/OrderServiceV1.java
@@ -0,0 +1,25 @@
+package me.doyoung.springcorehigh.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import me.doyoung.springcorehigh.trace.TraceStatus;
+import me.doyoung.springcorehigh.trace.hellotrace.HelloTraceV1;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
