6212507d5604fb510f8856ee967fb6451a37ec35
김도영 <doyoung0205@naver.com>
2022-01-31 15:55:52 +0900

    feat (LogTrace) : apply template method pattern

---
+++ src/main/java/me/doyoung/springcorehigh/app/v4/OrderControllerV4.java
@@ -0,0 +1,27 @@
+package me.doyoung.springcorehigh.app.v4;
+
+import lombok.RequiredArgsConstructor;
+import me.doyoung.springcorehigh.trace.logtrace.LogTrace;
+import me.doyoung.springcorehigh.trace.template.AbstractTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV4 {
+    private final OrderServiceV4 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v4/request")
+    public String request(String itemId) {
+
+        final AbstractTemplate<String> template = new AbstractTemplate<>(trace) {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        };
+        return template.execute("OrderController.request");
+    }
+}
