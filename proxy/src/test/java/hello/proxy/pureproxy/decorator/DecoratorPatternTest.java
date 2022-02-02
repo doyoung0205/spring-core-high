27623d2ef3abf727c4aa2fed7332612b64148e68
김도영 <doyoung0205@naver.com>
2022-02-02 09:02:46 +0900

    docs : add decorator pattern

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,37 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        final RealComponent realComponent = new RealComponent();
+        final DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        final MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        final DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        final MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        final TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        final DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
