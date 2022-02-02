3736eaf1ea0d3f13c4ade6f3120e2aca9ee49ffe
김도영 <doyoung0205@naver.com>
2022-02-02 14:22:18 +0900

    feat : java cglib 추가 및 문서 정리

---
+++ proxy/src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,22 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+    @Test
+    void cglib() {
+        ConcreteService target = new ConcreteService();
+        final Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        final ConcreteService proxy = (ConcreteService) enhancer.create();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+        proxy.call();
+    }
+}
