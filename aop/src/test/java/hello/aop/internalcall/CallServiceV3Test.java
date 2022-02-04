ee3b78a60f2ef6b2551f769f001589ffcd4579da
김도영 <doyoung0205@naver.com>
2022-02-04 21:25:52 +0900

    feat : add aop 프록시와 내부 호출 - 대안3 구조 변경 추가

---
+++ aop/src/test/java/hello/aop/internalcall/CallServiceV3Test.java
@@ -0,0 +1,27 @@
+package hello.aop.internalcall;
+
+import hello.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV3Test {
+
+    @Autowired
+    CallServiceV3 callServiceV3;
+
+    @Test
+    void external() {
+        log.info("target={}", callServiceV3.getClass());
+        // 해당 메서드 안에 있는 internal 도 포인트컷에 걸리는데 어디바이스가 적용되지 않음.
+        // 프록시의 internal 을 호출하지 않고, 타겟 객체의 internal 을 호출 하기 때문.
+        callServiceV3.external();
+    }
+
+}
